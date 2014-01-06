68f090fb02328c2bcd75ae14af8ef26489b7b7e0
unknown <hisconer@gmail.com>
2014-01-06 22:54:12 +0100

    Bug fixed with HTML Sheets and image slicing

--- pynav.py
+++ pynav.py
@@ -42,9 +42,9 @@ import json
 #	Functions
 #
 
-# Loads into settingDic the settings found in the config file
 def loadSettings(settingDic):
-	pynavConfigFile = "pynav.json"
+	""" Loads into settingDic the settings found in the config file """
+	pynavConfigFile = "pynav-conf/pynav.conf"
 	if os.path.isfile(pynavConfigFile):
 		confFile = open(pynavConfigFile, 'r')
 		jsonConfFile = json.load(confFile)
@@ -52,10 +52,21 @@ def loadSettings(settingDic):
 		for key in jsonConfFile["userSettings"].keys():
 			settingDic[key] = jsonConfFile["userSettings"][key]
 
-# Returns the maximun copy number (string) of an folder list based on a name
+def loadSheets(sheetFile):
+	""" Returns a string with the content o file """
+	f = open(sheetFile, "r")
+	content = f.read()
+	f.close()
+
+	if "[pynav-img]" not in content:
+		return False
+
+	return content
+
 def getMaxTrailNumber(baseName, dirList):
-	# There is folder(s) with trails (n)
+	""" Returns the maximun copy number (string) of an folder list based on a name """
 	try:
+		# There is folder(s) with trails (n)
 		baseNameList = [d for d in dirList if d.startswith(baseName+"(")]
 		trailsNumbers = []
 		for d in baseNameList:	
@@ -66,71 +77,69 @@ def getMaxTrailNumber(baseName, dirList):
 		# There is not folders with copy number, just the same folder name. Then max copy number = (1)
 		return "1"
 
-# Returns a next copy directory name if name == name(1). if name(12) == name(13)
 def resolveConflict(myDir, dirPath):
+	"""	Returns a next copy directory name if name == name(1). if name(12) == name(13) """
 	listDir = getListDir(dirPath)
 	if myDir in listDir:
 		return getMaxTrailNumber(myDir, listDir)
 	else:
 		return myDir
 
-# Gets file list with custom extension
 def getTypeFromFolder(folder, ImageType):
+	""" Gets file list with custom extension """
 	from os import listdir
 	from os.path import isfile, join	
 	return [ "%s/%s" % (folder, f) for f in listdir(folder) if isfile(join(folder,f)) and f[-3:] == ImageType]
 
-# Shifts list items by n
 def shift(seq, n):
-    n = n % len(seq)
-    return seq[n:] + seq[:n]
+	""" Shifts list items by n """
+	n = n % len(seq)
+	return seq[n:] + seq[:n]
 
-# Determines the image type of fhandle and return its size
 def get_image_size(fname):
-    fhandle = open(fname, 'rb')
-    head = fhandle.read(24)
-    if len(head) != 24:
-        return
-    if imghdr.what(fname) == 'png':
-        check = struct.unpack('>i', head[4:8])[0]
-        if check != 0x0d0a1a0a:
-            return
-        width, height = struct.unpack('>ii', head[16:24])
-    elif imghdr.what(fname) == 'gif':
-        width, height = struct.unpack('<HH', head[6:10])
-    elif imghdr.what(fname) == 'jpeg':
-        try:
-            fhandle.seek(0) # Read 0xff next
-            size = 2
-            ftype = 0
-            while not 0xc0 <= ftype <= 0xcf:
-                fhandle.seek(size, 1)
-                byte = fhandle.read(1)
-                while ord(byte) == 0xff:
-                    byte = fhandle.read(1)
-                ftype = ord(byte)
-                size = struct.unpack('>H', fhandle.read(2))[0] - 2
-            # We are at a SOFn block
-            fhandle.seek(1, 1)  # Skip `precision' byte.
-            height, width = struct.unpack('>HH', fhandle.read(4))
-        except Exception: #IGNORE:W0703
-            return
-    else:
-        return
-    return width, height
+	""" Determines the image type of fhandle and return its size """
+	fhandle = open(fname, 'rb')
+	head = fhandle.read(24)
+	if len(head) != 24:
+		return
+	if imghdr.what(fname) == 'png':
+		check = struct.unpack('>i', head[4:8])[0]
+		if check != 0x0d0a1a0a:
+			return
+		width, height = struct.unpack('>ii', head[16:24])
+	elif imghdr.what(fname) == 'gif':
+		width, height = struct.unpack('<HH', head[6:10])
+	elif imghdr.what(fname) == 'jpeg':
+		try:
+			fhandle.seek(0) # Read 0xff next
+			size = 2
+			ftype = 0
+			while not 0xc0 <= ftype <= 0xcf:
+				fhandle.seek(size, 1)
+				byte = fhandle.read(1)
+				while ord(byte) == 0xff:
+					byte = fhandle.read(1)
+				ftype = ord(byte)
+				size = struct.unpack('>H', fhandle.read(2))[0] - 2
+			# We are at a SOFn block
+			fhandle.seek(1, 1)  # Skip `precision' byte.
+			height, width = struct.unpack('>HH', fhandle.read(4))
+		except Exception: #IGNORE:W0703
+			return
+	else:
+		return
+	return width, height
 
-# Returns List of folders
 def getListDir(path):
-
+	""" Returns List of folders """
 	return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 
-# Returns List of files
 def getFileList(path):
-
+	""" Returns List of files """
 	return [d for d in os.listdir(path) if not os.path.isdir(os.path.join(path, d))]
 
-# zip files in a src with dst name
 def zip(src, dst):
+	""" zip files in a src with dst name """
 	if os.path.isfile(dst):
 		os.remove(dst)
 
@@ -142,8 +151,10 @@ def zip(src, dst):
 		zf.write(os.path.abspath("%s/%s" % (abs_src, f)), os.path.basename(f))
 	zf.close()
 
-# PREVIZ!
 def makePrevizNav(settings, userSettings):
+	"""
+		Main Function
+	"""
 	# timing!
 	start = time.clock()	
 
@@ -184,9 +195,9 @@ def makePrevizNav(settings, userSettings):
 	# User custon names
 	if settings["fileName"] != None:
 		# previz\user_custom_file_name.jpg
-		imgsFullPath = [ "%s\\%s_%02d.%s" % (settings["destinationPath"], settings["fileName"], n, settings["outputFormat"]) for n in range(len(sourceImagesFullPath)) ]
+		imgsFullPath = ["%s\\%s_%02d.%s" % (settings["destinationPath"], settings["fileName"], n, settings["outputFormat"]) for n in range(len(sourceImagesFullPath))]
 		# previz\user_custom_file_name.html
-		htmlsFullPath = [ "%s\\%s_%02d.html" % (settings["destinationPath"], settings["fileName"], n) for n in range(len(sourceImagesFullPath)) ]		
+		htmlsFullPath = ["%s\\%s_%02d.html" % (settings["destinationPath"], settings["fileName"], n) for n in range(len(sourceImagesFullPath))]
 	else:
 		# previz\original_name.jpg
 		imgsFullPath = [settings["destinationPath"] + "/" + "%s%s" % (f, "."+settings["outputFormat"]) for f in [os.path.basename(f[:-4]) for f in sourceImagesFullPath]]
@@ -224,9 +235,9 @@ def makePrevizNav(settings, userSettings):
 			pattern = re.compile(r'\s+')
 			customCss = re.sub(pattern, '', settings["css"])
 			customCss = customCss.split("}")[:-1]
-			customCss = [((("\t\t%s}\n"%settings["css"]).replace("{", " {\n\t\t\t")).replace(";",";\n\t\t\t")).replace("\n\t\t\t}","\n\t\t}\n") for settings["css"] in customCss]
+			customCss = [((("\t\t%s}"%settings["css"]).replace("{", " {\n\t\t\t")).replace(";",";\n\t\t\t")).replace("\n\t\t\t}","\n\t\t}") for settings["css"] in customCss]
 			customCss = "".join(customCss)
-			customCss = "\t\t/* CSS Style Command Inline*/\n" + customCss
+			customCss = "/* CSS Style Command Inline*/\n" + customCss
 		else:
 			customCss = ""
 
@@ -241,72 +252,79 @@ def makePrevizNav(settings, userSettings):
 				else:
 					os.remove(content)
 
-
+		# File by file
 		for i in range(filesToConvert):
-			# psd > img
-			inFile = sourceImagesFullPath[i]+'[0]'
+
+			inFile = sourceImagesFullPath[i]+'[0]' # add [0] to flatten psds for convert app
 			outFile = imgsFullPath[i]
 			
+			# If file exists and overwrite == False, skip.
 			fileExists = os.path.isfile(outFile)
 			if fileExists and settings["overwrite"] == False:
 				# msg = "Skip %s already exists" % os.path.basename(outFile)
 				if settings["fullPath"]:
 					msg = "%03d%% ... %s (Skip)" % ((float((100.0/filesToConvert))*(i+1)), inFile[:-3])
 				else:
-					msg = "%03d%% ... %s (Skip)" % ((float((100.0/filesToConvert))*(i+1)), os.path.basename(inFile)[:-3])					
+					msg = "%03d%% ... %s (Skip)" % ((float((100.0/filesToConvert))*(i+1)), os.path.basename(inFile)[:-3])
 				print msg
 				logText_c += msg+"\n"
-			else:												
-				# desktop
-				if settings["mobile"] == False:
-					startConvertFile = time.clock()
-					subprocess.call( [userSettings["convert_app"], '-quality', str(settings["quality"]), inFile, outFile], shell=False )
-					height = str(get_image_size(outFile)[1])
-				# mobile
+
+			# Ok, go to write some files
+			else:
+
+				# Si hemos indicado --mobile cambiamos la plantilla a mobile.html
+				if settings["mobile"] == True:
+					Convert_HTML_sheet = settings["mobileSheet"]
 				else:
-					startConvertFile = time.clock()
-					# Create a temp directory
-					tmp = "%s/previz_temp" % settings["destinationPath"]
-					if not os.path.exists(tmp):
-						os.makedirs(tmp)
+					Convert_HTML_sheet = settings["desktopSheet"]							
+				
+				# Procesamos los archivos.
+				startConvertFile = time.clock()
 
-					# creamos un jpg low qualy para ver el alto
-					subprocess.call( [userSettings["convert_app"], '-quality', '1', inFile, "%s/.__temp_previz.jpg" % tmp ], shell=False )
-					width = str(get_image_size("%s/.__temp_previz.jpg" % tmp)[0])
-					height = str(get_image_size("%s/.__temp_previz.jpg" % tmp)[1])
+				# Create a temp directory
+				tmp = "%s/previz_temp" % settings["destinationPath"]
+				if not os.path.exists(tmp):
+					os.makedirs(tmp)
 
-					# import math
-					import math
-					nSlices = int(math.ceil(float(height)/float(settings["sliceSize"])))
+				# creamos un jpg low qualy para ver el alto
+				subprocess.call( [userSettings["convert_app"], '-quality', '1', inFile, "%s/.__temp_previz.jpg" % tmp ], shell=False )
+				width = str(get_image_size("%s/.__temp_previz.jpg" % tmp)[0])
+				height = str(get_image_size("%s/.__temp_previz.jpg" % tmp)[1])
+
+				# import math
+				import math
+				nSlices = int(math.ceil(float(height)/float(settings["sliceSize"])))
+				
+				imgTag = ""
+				imgTag2 = []
+				for slcs in range(nSlices):
+					if int(settings["sliceSize"]) > float(height)-(slcs*float(settings["sliceSize"])):
+						newSliceSize = float(height)-(slcs*float(settings["sliceSize"]))
+					else:
+						newSliceSize = settings["sliceSize"]
+
+					# change the output file name adding number for slice
+					if slcs == 0:
+						ofile = outFile
+					else:
+						ofile = "%s_slice_%s.%s" % (outFile[:-4], str(slcs), settings["outputFormat"])
+
+					# generate output files
+					crop = '%sx%s+%s+%s' % (int(width), int(newSliceSize), 0, int(slcs*settings["sliceSize"]))
 					
-					imgTag = ""
-					for slcs in range(nSlices):						
-						if int(settings["sliceSize"]) > float(height)-(slcs*float(settings["sliceSize"])):
-							newSliceSize = float(height)-(slcs*float(settings["sliceSize"]))
-						else:
-							newSliceSize = settings["sliceSize"]
+					subprocess.call(
+						[userSettings["convert_app"],
+						'-quality', str(settings["quality"]),
+						inFile,
+						'-crop', crop,
+						ofile],
+						shell=False
+					)
 
-						# change the output file name adding number for slice
-						if slcs == 0:
-							ofile = outFile
-						else:
-							ofile = "%s_slice_%s.%s" % (outFile[:-4], str(slcs), settings["outputFormat"])
-
-						# generate output files
-						crop = '%sx%s+%s+%s' % (int(width), int(newSliceSize), 0, int(slcs*settings["sliceSize"]))
-						
-						subprocess.call(
-							[userSettings["convert_app"],
-							'-quality', str(settings["quality"]),
-							inFile,
-							'-crop', crop,
-							ofile],
-							shell=False
-						)
-
-						# generate html img tag to includo into html file
-						imgTag += "\t\t\t<img src=\"%s\">\n" % os.path.basename(ofile)
-						#subprocess.call( [userSettings["convert_app"], '-quality', str(settings["quality"]), inFile, outFile], shell=False )			
+					# generate html img tag to includo into html file
+					#imgTag += "\t\t\t<img src=\"%s\">\n" % os.path.basename(ofile)	
+					imgTag += os.path.basename(ofile)
+					imgTag2.append(os.path.basename(ofile))
 
 				# If only wants images
 				if settings["onlyimage"] == False:
@@ -316,87 +334,37 @@ def makePrevizNav(settings, userSettings):
 					html = open(htmlFile, "w")
 
 					# Html params
-					height = height
-					webImageFile = os.path.basename(outFile)
 					nextHtmlFile = os.path.basename(targetFile)
+					webImageFile = os.path.basename(outFile)
 
-					# Html markup desktop
-					if settings["mobile"] == False:
-						html.write('\
-\n<!--\
-\n\
-\n\tPynav 2014\
-\n\tFrancis Vega\
-\n\
-\n\thisco@inartx.com\
-\n\
-\n-->\
-\n\
-\n<!DOCTYPE html>\
-\n<html>\
-\n\t<head>\
-\n\t<title>' + settings["title"] + '</title>\
-\n\
-\n\t<style>\
-\n\n\t\t/* Pynav default style*/\
-\n\t\t* {\
-\n\t\t\tpadding:0;\
-\n\t\t\tmargin:0;\
-\n\t\t}\
-\n\t\t\
-\n\t\tdiv {\
-\n\t\t\theight:' + height + 'px;\
-\n\t\t\tmargin:0 auto;\
-\n\t\t\tbackground:url("' + webImageFile + '") top center no-repeat;\
-\n\t\t}\
-\n\
-\n' + customCss + '\
-\n\t</style>\
-\n\t</head>\
-\n\t<body>\
-\n\t\t<a href="' + nextHtmlFile + '"><div></div></a>\
-\n\t</body>\
-\n</html>')
-					else:
-						html.write('\
-\n<!--\
-\n\
-\n\tPynav 2014\
-\n\tFrancis Vega\
-\n\
-\n\thisco@inartx.com\
-\n\
-\n-->\
-\n\
-\n<!DOCTYPE html>\
-\n<html>\
-\n\t<head>\
-\n\t<title>' + settings["title"] + '</title>\
-\n\
-\n\t<style>\
-\n\n\t\t/* Pynav default style*/\
-\n\t\t* {\
-\n\t\t\tpadding:0;\
-\n\t\t\tmargin:0;\
-\n\t\t}\
-\n\
-\n\t\t\
-\n\t\timg {\
-\n\t\t\twidth:100%;\
-\n\t\t\theight:auto;\
-\n\t\t\tdisplay:block;\
-\n\t\t}\
-\n\
-\n\
-\n' + customCss + '\
-\n\t</style>\
-\n\t</head>\
-\n\t<body>\
-\n\t\t<a href="' + nextHtmlFile + '">\n'+ imgTag +'\t\t</a>\
-\n\t</body>\
-\n</html>')
+					# replace custom tags
+					tags = Convert_HTML_sheet
 
+					tags = tags.replace("[pynav-title]", settings["title"])
+					tags = tags.replace("[pynav-css]", customCss)
+					tags = tags.replace("[pynav-img-width]", width)
+					tags = tags.replace("[pynav-img-height]", height)
+					tags = tags.replace("[pynav-next-html]", nextHtmlFile)			
+
+					tags = tags.replace("[pynav-img]", imgTag2[0])
+					
+					if nSlices > 1:
+						for i in range(nSlices-1):
+							tags = tags.replace("[pynav-img-slice-%s]" % str(i+1), imgTag2[i+1])
+					
+					html.write(tags)
 					html.close()
+
+					# Remove [pynav-img-slice-n] tags
+					temporalHTML = "%s/tempo.html" % (settings["destinationPath"])
+					shutil.copy(htmlFile, temporalHTML)
+					regex = re.compile('.*pynav-img-slice-.*')
+					f = open(htmlFile, "w")
+					map(lambda x: f.write(x), filter(lambda x: not regex.match(x), open(temporalHTML)))
+					f.close()
+
+					# Remove temporal file
+					os.remove(temporalHTML)
 
 				if settings["fullPath"]:
 					inFile = inFile[:-3]
@@ -421,7 +389,10 @@ def makePrevizNav(settings, userSettings):
 			indexHTML += "\t<li><a href='%s'>%s</a></li>\n" % (os.path.basename(htmlsFullPath[i]), os.path.basename(outFile)[:-4])
 		
 		
-		indexHTML = "\n<!--\n\n\tPynav 2014\n\tFrancis Vega\n\n\thisco@inartx.com\n\n-->\n\n<!DOCTYPE html>\n<html>\n<head>\n<title>Index of "+ settings["title"] +"</title>\n<style>\n\ta:visited{color: inherit;}\n\ta{color: black; text-decoration: none}\n\ta:hover{color: black; font-weight: bold;}\n\th1{margin: 20px 0 0 20px;}\n\tli{line-height: 1.6; }\n\tul{list-style: none; margin: 20px 0 0 20px;}\n\t*{font-family: Arial; border: 0; margin: 0; padding:0;}\n" + customCss + "</style>\n</head>\n<h1>Index of "+ settings["title"] +"</h1>\n<ul>\n%s</ul>\n</body>\n</html>" % indexHTML
+		indexHTML = "\
+<!--\
+\n\n\tPynav 2014\
+\n\tFrancis Vega\n\n\thisco@inartx.com\n\n-->\n\n<!DOCTYPE html>\n<html>\n<head>\n<title>Index of "+ settings["title"] +"</title>\n<style>\n\ta:visited{color: inherit;}\n\ta{color: black; text-decoration: none}\n\ta:hover{color: black; font-weight: bold;}\n\th1{margin: 20px 0 0 20px;}\n\tli{line-height: 1.6; }\n\tul{list-style: none; margin: 20px 0 0 20px;}\n\t*{font-family: Arial; border: 0; margin: 0; padding:0;}\n" + customCss + "</style>\n</head>\n<h1>Index of "+ settings["title"] +"</h1>\n<ul>\n%s</ul>\n</body>\n</html>" % indexHTML
 
 	except KeyboardInterrupt:
 		logText_e += "\n"
@@ -456,6 +427,74 @@ def makePrevizNav(settings, userSettings):
 
 	if settings["zip"]:
 		zip(settings["destinationPath"], "%s/%s.zip" % (settings["destinationPath"], os.path.basename(settings["destinationPath"])))
+
+#
+# html sheets
+#
+
+desktopSheet ="\
+\n<!DOCTYPE html>\
+\n<html>\
+\n	<head>\
+\n	<title>[pynav-title]</title>\
+\n	<style>\
+\n		/* Pynav default style*/\
+\n		* {\
+\n			padding:0;\
+\n			margin:0;\
+\n		}\
+\n		div {\
+\n			margin:0 auto;\
+\n			background:url('[pynav-img]') top center no-repeat;\
+\n			height:[pynav-img-height]px;\
+\n			width:[pynav-img-width]px;\
+\n		}\
+\n		[pynav-css]\
+\n	</style>\
+\n	</head>\
+\n	<body>\
+\n		<a href='[pynav-next-html]'><div></div></a>\
+\n	</body>\
+\n</html>"
+
+
+mobileSheet = "\
+\n<!DOCTYPE html>\
+\n<html>\
+\n	<head>\
+\n	<title>[pynav-title]</title>\
+\n	<style>\
+\n		/* Pynav default style*/\
+\n		* {\
+\n			padding:0;\
+\n			margin:0;\
+\n		}\
+\n		img {\
+\n			width:100%;\
+\n			height:auto;\
+\n			display:block;\
+\n		}\
+\n		[pynav-css]\
+\n	</style>\
+\n	</head>\
+\n	<body>\
+\n		<a href='[pynav-next-html]'>\
+\n			<img src='[pynav-img]'>\
+\n			<img src='[pynav-img-slice-1]'>\
+\n			<img src='[pynav-img-slice-2]'>\
+\n			<img src='[pynav-img-slice-3]'>\
+\n			<img src='[pynav-img-slice-4]'>\
+\n			<img src='[pynav-img-slice-5]'>\
+\n		</a>\
+\n	</body>\
+\n</html>"
+
+# Load file sheets
+if os.path.isfile('desktop.html'):
+	desktopSheet = loadSheets('pynav-desktop.html')
+
+if os.path.isfile('mobile.html'):
+	mobileSheet = loadSheets('pynav-mobile.html')
 
 #
 #	Start the dance!
@@ -502,6 +541,8 @@ PARSER.add_argument( "--slice", "-slc", nargs=1, dest="slice", default=userSetti
 PARSER.add_argument( "--css-style", "-style", nargs=1, dest="css", default="", type=str, help="Add css style to all html files")
 PARSER.add_argument( "--zip", "-z", dest="zip", action="store_true", help="Create a zip file with results files" )
 PARSER.add_argument( "--flush", "-f", dest="flush", action="store_true", help="Delete all the content in the destination folder" )
+PARSER.add_argument( "--html-sheet", "-html", nargs=1, dest="html", default="", type=str, help="Use a custom html file")
+# PARSER.add_argument( "--list-html-tags", "-tags", nargs=1, dest="html", default="", type=str, help="Show a list of pynav html tags")
 
 # Gets parse arguments
 args = PARSER.parse_args()
@@ -558,10 +599,31 @@ settings["onlyimage"] = args.onlyimage
 settings["flush"] = args.flush
 settings["sliceSize"] = args.slice[0]
 settings["css"] = "".join(args.css)
+settings["html"] = "".join(args.html)
+
 if args.filename == None:
 	settings["fileName"] = None
 else:
-	settings["fileName"] = args.filename[0]	
+	settings["fileName"] = args.filename[0]
+
+# html sheet
+settings["mobileSheet"] = mobileSheet
+settings["desktopSheet"] = desktopSheet
+
+if settings["html"]:
+	if os.path.isfile("%s/%s" % (os.getcwd(), settings["html"])):
+		htmlSheet = loadSheets(settings["html"])
+		if htmlSheet:
+			settings["mobileSheet"] = htmlSheet
+			settings["desktopSheet"] = htmlSheet
+		else:
+			print "\nError. El archivo %s no tiene un formato de etiquetas adecuado" % settings["html"]
+			sys.exit()
+	else:
+		print "\nError. No existe el archivo html %s" % settings["html"]
+		sys.exit()
+else:
+	print "no se ha indicado html"
 
 # Go with the flow!!
 makePrevizNav(settings, userSettings)
