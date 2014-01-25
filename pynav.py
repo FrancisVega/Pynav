b92b26f34941f911983a54a1c8a20f4c229bcc9b
unknown <hisconer@gmail.com>
2014-01-25 17:57:07 +0100

    adding doc

--- pynav.py
+++ pynav.py
@@ -1,27 +1,43 @@
 """Pynav.
 
 Usage:
-  pynav.py <src> [<dst>] [-iwz] [--quality=QUALITY] [--output-format=FORMAT] [--input-format=FORMATe]
-  pynav.py set (--quality=QUALITY|--mode=(img|back))
-  pynav.py --version
+  pynav create <src> [<dst>] [-iwz] [--quality=QUALITY] [--output=FORMAT] [--input=FORMAT]
+               [--mode=(img|back)] [--html=FILE] [--css=STYLE]
+  pynav set [--quality=QUALITY]
 
 Commands:
-  set                           Write into the config file
+  create                        Main command to create navigation
+  set                           Command to write default values into config file
+
+Arguments:
+  <src>                         Source directory with image files
+  <dst>                         Destination directory to write output files
+  QUALITY                       Integer between (1-100)
+  FILE                          Valid file name
+  STYLE                         Valid css style
+  FORMAT                        Image format (jpg|png)
 
 Options:
   -h --help                     show this help message and exit
   --version                     show version and exit
   -i --index                    Create a index.html containing all pages
   -w --overwrite                Overwrite existings files
-  --FLUSH                       Clean target directory (risky)
-  -f --input-format=FORMATe     [default: psd]
-  -o --output-format=FORMAT     [default: jpg]
+  -z --zip                      Archive output files
+  -f --input=FORMAT             [default: psd]
+  -o --output=FORMAT            [default: jpg]
   -q --quality=QUALITY          [default: 99]
-  -m --mode=(img|back)          Modes [default: img]
+  --mode=(img|back)             Modes [default: img]
+  --html=FILE                   Use FILE (html) template
+  --css=STYLE                   Add custom css styles
+
+Examples:
+  pynav create d:/Dropbox/Secuoyas/web/visual/ -iwz
+  pynav set --quality=20 --css=body { background: #000000; }
 
 """
 from docopt import docopt
 
 
-args = docopt(__doc__, version='Naval Fate 2.0')
-print(args)
+args = docopt(__doc__, version='Pynav 0.1')
+# args = docopt(__doc__, argv="create d:/Dropbox/Secuoyas/web/visual/ d:/jps/ -iwz -q80", version='Pynav 0.1')
+print (args)
